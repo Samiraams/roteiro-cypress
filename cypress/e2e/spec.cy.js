1dbbcb47bdabca0ec21b1d896f5f2c0ce2dac095
Samira Malaquias <131280444+Samiraams@users.noreply.github.com>
2025-06-08 23:02:10 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,45 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+  it('Não adiciona tarefa vazia', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('{enter}');
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
+
+  it('Limpa o campo de entrada após adicionar uma tarefa', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('Teste limpeza{enter}');
+    cy.get('[data-cy=todo-input]')
+      .should('have.value', '');
+  });
+
+  it('Limpa apenas as tarefas completadas ao clicar em "Clear Completed"', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa A{enter}')
+      .type('Tarefa B{enter}');
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .find('[data-cy=toggle-todo-checkbox]')
+      .click();
+
+    // Novo seletor por texto do botão
+    cy.contains('button', 'Clear completed').click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Tarefa B');
+  });
+
+
+
 });
